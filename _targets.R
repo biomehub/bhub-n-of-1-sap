8c519907710f6ae321d9b5cd713634964ad7e506
giuliano-cruz <cruz.gnf@gmail.com>
2023-11-23 18:17:45 -0300

    final sim

--- _targets.R
+++ _targets.R
@@ -5,13 +5,13 @@ tar_option_set(
 )
 tar_source()
 
-overall_seed <- 23112023
+overall_seed <- 24112023
 
 list(
   tar_target(
     name = power_simulation,
     command = run_power_simulation(
-      n_sim = 20,
+      n_sim = 2000,
       n_threads = 10,
       overall_seed = overall_seed,
       overwrite = FALSE,
